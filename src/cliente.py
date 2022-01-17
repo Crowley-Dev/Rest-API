0f85fa3224003384e5416956e6c71715cb1a1966
Crowley <lg9064852@gmail.com>
2022-01-17 05:06:22 -0300

    Update cliente.py

--- src/cliente.py
+++ src/cliente.py
@@ -1,13 +1,16 @@
+#!/usr/bin/env python3
+
 import requests, json
 
 
 url = "http://0.0.0.0:5000/api?cpf={}"
+
 resp = requests.request(
-	method = "GET",
-	url = url.format("00000000272"),
-	headers = {
-		"Content-Type": "application/json",
-		"token": "6m8x4x0b-8d3z1o3j-7n8c3z4l-0b7m7r5h",
-	}
+    method = "GET",
+    url = url.format("00000000272"),
+    headers = {
+        "Content-Type": "application/json",
+        # "token": "6m8x4x0b-8d3z1o3j-7n8c3z4l-0b7m7r5h",
+    }
 
 ); print(resp.text)
